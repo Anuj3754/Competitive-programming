19550c11943d5f80d9326c6255e141555fc44809
Anuj Thakur <thakuranujsisodiya9@gmail.com>
2025-11-19 11:08:48 +0530

    Delete valid_triangle.py

--- valid_triangle.py
+++
@@ -1,9 +0,0 @@
-a = int(input("enter angle: "))
-b = int(input("enter angle : "))
-c = int(input("enter angle : "))
-
-if a+ b + c == 180:
-    print("Valid triangle")
-else:
-    print("Invalid triangle")
-  
