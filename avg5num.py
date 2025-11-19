568837c563cc9a401ea0a735df757b0a9b071e00
Anuj Thakur <thakuranujsisodiya9@gmail.com>
2025-11-19 11:11:31 +0530

    Delete avg5num.py

--- avg5num.py
+++
@@ -1,8 +0,0 @@
-a= int(input("enter number  "))
-b = int(input("enter number "))
-c = int(input("enter number  "))
-d = int(input("enter number  "))
-e = int(input("enter number  "))
-
-avg = (a + b + c + d + e) / 5
-print("average is:", avg)
